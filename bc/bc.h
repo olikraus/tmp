790722cd64678f25907cc7177d73efbec0ad47ca
kraus <olikraus@gmail.com>
2024-03-10 19:49:56 +0100

    restructure

---
+++ bc/bc.h
@@ -0,0 +1,189 @@
+/*
+
+  bc.h
+  
+  boolean cube
+
+*/
+
+#ifndef BC_H
+#define BC_H
+
+#include <x86intrin.h>
+#include <stdint.h>
+
+
+
+typedef struct bcp_struct *bcp;
+typedef __m128i *bc;
+typedef struct bcl_struct *bcl;
+
+
+/* boolean cube problem, each function will require a pointer to this struct */
+#define BCP_MAX_STACK_FRAME_DEPTH 500
+struct bcp_struct
+{
+  int var_cnt;  // number of variables per cube
+  int blk_cnt;  // number of blocks per cube, one block is one __m128i = 64 variables
+  int vars_per_blk_cnt; // number of variables per block --> 64
+  int bytes_per_cube_cnt; // number of bytes per cube, this is blk_cnt*sizeof(__m128i)
+  char *cube_to_str;    // storage area for one visual representation of a cube
+  bcl stack_cube_list;    // storage area for temp cubes
+  int stack_frame_pos[BCP_MAX_STACK_FRAME_DEPTH];
+  int stack_depth;
+  bcl global_cube_list;    // storage area for temp cubes
+};
+
+/* one cube, the number of __m128i is (var_cnt / 64) */
+
+struct bcl_struct
+{
+  int cnt;
+  int max;
+  int last_deleted;
+  __m128i *list;        // max * var_cnt / 64 entries
+  uint8_t *flags;       // bit 0 is the cube deleted flag
+};
+
+/*============================================================*/
+
+/* bcp.c */
+int bcp_GetVarCntFromString(const char *s);
+
+bcp bcp_New(size_t var_cnt);
+void bcp_Delete(bcp p);
+//bc bcp_GetGlobalCube(bcp p, int pos);
+#define bcp_GetGlobalCube(p, pos) \
+  bcp_GetBCLCube((p), (p)->global_cube_list, (pos))  
+void bcp_CopyGlobalCube(bcp p, bc r, int pos);
+void bcp_StartCubeStackFrame(bcp p);
+void bcp_EndCubeStackFrame(bcp p);
+bc bcp_GetTempCube(bcp p);
+
+/* bcube.c */
+/* core functions */
+
+void bcp_ClrCube(bcp p, bc c);
+void bcp_CopyCube(bcp p, bc dest, bc src);
+int bcp_CompareCube(bcp p, bc a, bc b);
+
+
+void bcp_SetCubeVar(bcp p, bc c, unsigned var_pos, unsigned value);
+//unsigned bcp_GetCubeVar(bcp p, bc c, unsigned var_pos);
+#define bcp_GetCubeVar(p, c, var_pos) \
+  ((((uint16_t *)(c))[(var_pos)/8] >> (((var_pos)&7)*2)) & 3)
+
+const char *bcp_GetStringFromCube(bcp p, bc c);
+void bcp_SetCubeByStringPointer(bcp p, bc c,  const char **s);
+void bcp_SetCubeByString(bcp p, bc c, const char *s);
+
+/* boolean functions */
+
+int bcp_IsTautologyCube(bcp p, bc c);
+int bcp_IntersectionCube(bcp p, bc r, bc a, bc b); // returns 0, if there is no intersection
+int bcp_IsIntersectionCube(bcp p, bc a, bc b); // returns 0, if there is no intersection
+int bcp_IsIllegal(bcp p, bc c);                                 // check whether "c" contains "00" codes
+int bcp_GetCubeVariableCount(bcp p, bc cube);   // return the number of 01 or 10 codes in "cube"
+int bcp_GetCubeDelta(bcp p, bc a, bc b);                // calculate the delta between a and b
+int bcp_IsSubsetCube(bcp p, bc a, bc b);                // is "b" is a subset of "a"
+
+
+
+bcl bcp_NewBCL(bcp p);          // create new empty bcl
+bcl bcp_NewBCLByBCL(bcp p, bcl l);      // create a new bcl as a copy of an existing bcl
+int bcp_CopyBCL(bcp p, bcl a, bcl b);
+void bcp_DeleteBCL(bcp p, bcl l);
+//bc bcp_GetBCLCube(bcp p, bcl l, int pos);
+#define bcp_GetBCLCube(p, l, pos) \
+  (bc)(((uint8_t *)((l)->list)) + (pos) * (p)->bytes_per_cube_cnt)
+void bcp_ShowBCL(bcp p, bcl l);
+void bcp_PurgeBCL(bcp p, bcl l);               /* purge deleted cubes */
+void bcp_DoBCLSingleCubeContainment(bcp p, bcl l);
+int bcp_IsBCLCubeCovered(bcp p, bcl l, bc c);           // is cube c a subset of l (is cube c covered by l)
+int bcp_IsBCLCubeRedundant(bcp p, bcl l, int pos);      // is the cube at pos in l covered by all other cubes in l
+void bcp_DoBCLMultiCubeContainment(bcp p, bcl l);
+void bcp_DoBCLSimpleExpand(bcp p, bcl l);
+void bcp_DoBCLExpandWithOffSet(bcp p, bcl l, bcl off);  // this operation does not do any SCC or MCC
+void bcp_DoBCLSubsetCubeMark(bcp p, bcl l, int pos);
+void bcp_DoBCLSharpOperation(bcp p, bcl l, bc a, bc b);
+
+void bcp_SubtractBCL(bcp p, bcl a, bcl b, int is_mcc);
+int bcp_IntersectionBCLs(bcp p, bcl result, bcl a, bcl b); // result = a intersection with b
+int bcp_IntersectionBCL(bcp p, bcl a, bcl b);   // a = a intersection with b 
+
+bcl bcp_NewBCLCofacterByVariable(bcp p, bcl l, unsigned var_pos, unsigned value);         // create a new list, which is the cofactor from "l"
+bcl bcp_NewBCLCofactorByCube(bcp p, bcl l, bc c, int exclude);          // don't use this fn, use bcp_IsBCLCubeRedundant() or bcp_IsBCLCubeCovered() instead
+
+int bcp_AddBCLCube(bcp p, bcl l);
+int bcp_AddBCLCubeByCube(bcp p, bcl l, bc c);
+int bcp_AddBCLCubesByBCL(bcp p, bcl a, bcl b);
+int bcp_AddBCLCubesByString(bcp p, bcl l, const char *s);
+#define bcp_GetBCLCnt(p, l) ((l)->cnt)
+void bcp_CalcBCLBinateSplitVariableTable(bcp p, bcl l);
+int bcp_GetBCLBalancedBinateSplitVariable(bcp p, bcl l);
+int bcp_GetBCLMaxBinateSplitVariable(bcp p, bcl l);
+int bcp_IsBCLUnate(bcp p);  // requires call to bcp_CalcBCLBinateSplitVariableTable
+
+int bcp_IsBCLTautology(bcp p, bcl l);
+int bcp_IsBCLSubsetWithCofactor(bcp p, bcl a, bcl b);   //   test, whether "b" is a subset of "a"
+int bcp_IsBCLSubsetWithSubstract(bcp p, bcl a, bcl b);  // this fn seems to be much slower than bcp_IsBCLSubsetWithCofactor
+bcl bcp_NewBCLComplementWithSubtract(bcp p, bcl l);  // faster than with cofactor
+bcl bcp_NewBCLComplementWithCofactor(bcp p, bcl l); // slow!
+void bcp_MinimizeBCL(bcp p, bcl l);
+
+/*
+static __m128i m128i_get_n_bit_mask(uint16_t val, unsigned bit_pos)
+{
+  uint16_t a[8] = { 0, 0, 0, 0, 0, 0, 0, 0 };
+  val <<= bit_pos & 0xf;
+  a[7-(bit_pos>>4)] = val;
+  return _mm_loadu_si128((__m128i *)a);
+}
+*/
+
+/*
+#define m128i_get_n_bit_mask(val, bit_pos) \
+  _mm_slli_si128(_mm_set_epi32(0, 0, 0, (val)<<(bit_pos&0x1f)), (bit_pos)>>5)
+*/
+
+/* limitation: the bitpattern in val must not cross a 32 bit boundery, for example val=3 and  bit_pos=31 is not allowed */
+/*
+#define m128i_get_n_bit_mask(val, bit_pos) \
+  _mm_set_epi32(  \
+    0==((bit_pos)>>5)?(((uint32_t)(val))<<(bit_pos&0x1f)):0, \
+    1==((bit_pos)>>5)?(((uint32_t)(val))<<(bit_pos&0x1f)):0, \
+    2==((bit_pos)>>5)?(((uint32_t)(val))<<(bit_pos&0x1f)):0, \
+    3==((bit_pos)>>5)?(((uint32_t)(val))<<(bit_pos&0x1f)):0 ) 
+*/
+
+/*
+#define m128i_get_1_bit_mask(bit_pos) \
+  m128i_get_n_bit_mask(1, bit_pos)
+*/
+
+/*
+#define m128i_get_bit_mask(bit_pos) \
+  _mm_slli_si128(_mm_set_epi32 (0, 0, 0, 1<<(bit_pos&0x1f)), (bit_pos)>>5)
+*/
+
+/*
+#define m128i_not(m) \
+  _mm_xor_si128(m, _mm_set1_epi16(0x0ffff))
+*/
+
+/*
+#define m128i_is_zero(m) \
+  ((_mm_movemask_epi8(_mm_cmpeq_epi16((m),_mm_setzero_si128())) == 0xFFFF)?1:0)
+*/
+
+#define m128i_is_equal(m1, m2) \
+  ((_mm_movemask_epi8(_mm_cmpeq_epi16((m1),(m2))) == 0xFFFF)?1:0)
+
+/* val=0..3, bit_pos=0..126 (must not be odd) */
+/*
+#define m128i_set_2_bit(m, val, bit_pos) \
+ _mm_or_si128( m128i_get_n_bit_mask((val), (bit_pos)), _mm_andnot_si128(m128i_get_n_bit_mask(3, (bit_pos)), (m)) )
+*/
+
+#endif
+
---
+++ bc/bcp.c
@@ -0,0 +1,146 @@
+/*
+
+  bcp.c
+  
+  boolean cube problem
+  
+*/
+
+#include "bc.h"
+#include <assert.h>
+#include <stdlib.h>
+#include <string.h>
+
+
+int bcp_GetVarCntFromString(const char *s)
+{
+  int cnt = 0;
+  for(;;)
+  {
+    while( *s == ' ' || *s == '\t' )            // skip white space
+      s++;
+    if ( *s == '\0' || *s == '\r' || *s == '\n' )       // stop looking at further chars if the line/string ends
+    {
+      return cnt;
+    }
+    s++;
+    cnt++;
+  }
+  return cnt; // we should never reach this statement
+}
+
+
+bcp bcp_New(size_t var_cnt)
+{
+  bcp p = (bcp)malloc(sizeof(struct bcp_struct));
+  if ( p != NULL )
+  {
+      p->var_cnt = var_cnt;
+      p->vars_per_blk_cnt = sizeof(__m128i)*4;
+      p->blk_cnt = (var_cnt + p->vars_per_blk_cnt-1)/p->vars_per_blk_cnt;
+      p->bytes_per_cube_cnt = p->blk_cnt*sizeof(__m128i);
+      //printf("p->bytes_per_cube_cnt=%d\n", p->bytes_per_cube_cnt);
+      p->stack_depth = 0;
+      p->cube_to_str = (char *)malloc(p->var_cnt+1);
+      if ( p->cube_to_str != NULL )
+      {
+        p->stack_cube_list = bcp_NewBCL(p);
+        if ( p->stack_cube_list != NULL )
+        {
+          p->global_cube_list = bcp_NewBCL(p);
+          if ( p->global_cube_list != NULL )
+          {
+            int i;
+			/*
+				0..3:	constant cubes for all illegal, all zero, all one and all don't care
+				4..7:	uint8_t counters for zeros in a list
+				8..11:	uint8_t counters for ones in a list
+                                4..11: uint16_t counters
+                                12..19: uint16_t counters
+			*/
+            for( i = 0; i < 4+8+8; i++ )
+              bcp_AddBCLCube(p, p->global_cube_list);
+            if ( p->global_cube_list->cnt >= 4 )
+            {
+              memset(bcp_GetBCLCube(p, p->global_cube_list, 0), 0, p->bytes_per_cube_cnt);  // all vars are illegal
+              memset(bcp_GetBCLCube(p, p->global_cube_list, 1), 0x55, p->bytes_per_cube_cnt);  // all vars are zero
+              memset(bcp_GetBCLCube(p, p->global_cube_list, 2), 0xaa, p->bytes_per_cube_cnt);  // all vars are one
+              memset(bcp_GetBCLCube(p, p->global_cube_list, 3), 0xff, p->bytes_per_cube_cnt);  // all vars are don't care
+              return p;
+            }
+            bcp_DeleteBCL(p, p->global_cube_list);
+          }
+          bcp_DeleteBCL(p, p->stack_cube_list);
+        }
+        free(p->cube_to_str);
+      }
+      free(p);
+  }
+  return NULL;
+}
+
+void bcp_Delete(bcp p)
+{
+  if ( p == NULL )
+    return ;
+  bcp_DeleteBCL(p, p->global_cube_list);
+  bcp_DeleteBCL(p, p->stack_cube_list);
+  free(p->cube_to_str);
+  free(p);
+}
+
+#ifndef bcp_GetGlobalCube
+bc bcp_GetGlobalCube(bcp p, int pos)
+{
+  assert(pos < p->global_cube_list->cnt && pos >= 0);
+  return bcp_GetBCLCube(p, p->global_cube_list, pos);  
+}
+#endif
+/*
+#define bcp_GetGlobalCube(p, pos) \
+  bcp_GetBCLCube((p), (p)->global_cube_list, (pos))  
+*/
+
+/* copy global cube at pos to cube provided in "r" */
+void bcp_CopyGlobalCube(bcp p, bc r, int pos)
+{
+  bcp_CopyCube(p, r, bcp_GetGlobalCube(p, pos));
+}
+
+
+/* start a new stack frame for bcp_GetTempCube() */
+void bcp_StartCubeStackFrame(bcp p)
+{
+  if ( p->stack_depth >= BCP_MAX_STACK_FRAME_DEPTH )
+  {
+    assert(p->stack_depth < BCP_MAX_STACK_FRAME_DEPTH);  // output error message
+    exit(1); // just ensure, that we do exit, also incases if NDEBUG is active
+  }
+  p->stack_frame_pos[p->stack_depth] = p->stack_cube_list->cnt;
+  p->stack_depth++;    
+}
+
+/* delete all cubes returned by bcp_GetTempCube() since corresponding call to bcp_StartCubeStackFrame() */
+void bcp_EndCubeStackFrame(bcp p)
+{
+  assert(p->stack_depth > 0);
+  p->stack_depth--;
+  p->stack_cube_list->cnt = p->stack_frame_pos[p->stack_depth] ;  // reduce the stack list to the previous size
+}
+
+/* Return a temporary cube, which will deleted with bcp_EndCubeStackFrame(). Requires a call to bcp_StartCubeStackFrame(). */
+/* this is NOT MT-SAFE, each thread requires its own bcp structure */
+bc bcp_GetTempCube(bcp p)
+{
+  int i;
+  assert(p->stack_depth > 0);
+  i = bcp_AddBCLCube(p, p->stack_cube_list);
+  if ( i < p->stack_frame_pos[p->stack_depth-1] )
+  {
+    assert( i >= p->stack_frame_pos[p->stack_depth-1] );
+    exit(1);
+  }
+  return bcp_GetBCLCube(p, p->stack_cube_list, i);
+}
+
+
